eccc1af7dd470e7ae26ee9ad25ae739c876ad74d
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-26 14:59:18 -0400

    rewrote main

--- movie-fe/src/main.tsx
+++ movie-fe/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// import React
+import React from 'react'
+// import ReactDOM to mount the app
+import ReactDOM from 'react-dom/client'
+// import our root app
 import App from './App.tsx'
+// import global styles (Tailwind + overrides)
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// mount the app in the #root element with strict mode for DX
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
